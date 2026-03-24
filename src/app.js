514a377225e50412eea79bf8f62758bb865fb6e3
Shafa <shafa@gmail.com>
2026-03-24 15:00:27 +0700

    1. add admin bull dashboard URL and mount it 2. add queue adn redis

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
+import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+import { Queue } from 'bullmq';
+import Redis from 'ioredis';
 import express from 'express';
 import apiRoutes from './routes/api.routes.js';
 
 const app = express();
-
 app.use(express.json());
 
+const redis = new Redis({ host: 'redis' });
+const queue = new Queue('orders', { connection: redis });
+
+// setup server adapter
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+// init bull board
+createBullBoard({
+  queues: [new BullMQAdapter(queue)],
+  serverAdapter,
+});
+
 app.use('/api', apiRoutes);
+// mount ke express
+app.use('/admin/queues', serverAdapter.getRouter());
 
 export default app;
