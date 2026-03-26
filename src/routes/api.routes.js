0796cf587f0260ddd9cd188ca67922e8baa72230
M Latief <latief.polcode@gmail.com>
2026-03-26 07:09:25 +0700

    add stock endpoint to retrieve product stock from Redis

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -2,11 +2,17 @@ import express from 'express';
 import { getMessage } from '../controllers/api.controller.js';
 import { buyProductKafka } from '../controllers/kafkaOrder.controller.js';
 import { buyProductBull } from '../controllers/order.controller.js';
+import Redis from 'ioredis';
 
+const redis = new Redis({ host: 'redis' });
 const router = express.Router();
 
 router.get('/', getMessage);
 router.get('/buy', buyProductKafka);
 router.get('/buy-bull', buyProductBull);
+router.get('/stock', async (req, res) => {
+  const stock = await redis.get('stock:product1');
+  res.send({ stock });
+});
 
 export default router;
