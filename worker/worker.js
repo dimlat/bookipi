acba0e96db3289d0b606a88aa00ab10550fe8043
M Latief <latief.polcode@gmail.com>
2026-03-26 21:43:31 +0700

    feat: refactor worker to use centralized DB connection and Order model

--- worker/worker.js
+++ worker/worker.js
@@ -1,28 +1,11 @@
 import { Worker } from 'bullmq';
-import Redis from 'ioredis';
-import mongoose from 'mongoose';
-
+import redis from "../src/lib/redis.js";
 import { keys } from '../src/services/flashSale.service.js';
-
-await mongoose.connect('mongodb://mongodb:27017/shop');
-
-const OrderSchema = new mongoose.Schema({
-    userId: String,
-    productId: String,
-    createdAt: { type: Date, default: Date.now }
-});
-
-OrderSchema.index({ userId: 1, productId: 1 }, { unique: true });
-
-const Order = mongoose.model('Order', OrderSchema);
+import { connectDB } from '../src/config/db.js';
+import Order from '../src/models/Order.js';
 
 console.log("🚀 Worker starting...");
 
-const redis = new Redis({
-    host: 'redis',
-    maxRetriesPerRequest: null,
-    enableReadyCheck: false
-});
 const worker = new Worker(
     'orders',
     async job => {
@@ -38,7 +21,15 @@ const worker = new Worker(
 
         // DB writing
         try {
-            const newOrder = await Order.create({ userId: job.data.userId, productId: job.data.productId });
+            await connectDB();
+            await Order.create({ userId: job.data.userId, productId: job.data.productId });
+            const userKey = keys.userBought(job.data.userId);
+            // tandai user sudah beli
+            await redis.set(userKey, '1', 'EX', 3600);
+            await redis.lpush('purchases', JSON.stringify({
+                userId: job.data.userId,
+                time: new Date().toISOString()
+            }));
         } catch (error) {
             if (error.code === 11000) {
                 console.log('⚠️ Duplicate DB insert prevented');
@@ -46,10 +37,6 @@ const worker = new Worker(
             }
             throw error;
         }
-
-        const userKey = keys.userBought(job.data.userId);
-        // tandai user sudah beli
-        await redis.set(userKey, '1', 'EX', 3600);
 
         // RETURN hasilnya agar muncul di dashboard
         return {
@@ -75,6 +62,11 @@ worker.on('failed', (job, err) => {
     } else {
         console.log(`⚠️ Failed job ${job.id}, retrying... (attempt ${job.attemptsMade})`);
     }
+
+    // 🔥 ini yang kamu mau
+    console.error('❌ Error message:', err.message);
+    console.error('📦 Job data:', job.data);
+    console.error('🧵 Stack trace:', err.stack);
 });
 
 worker.on('error', err => {
