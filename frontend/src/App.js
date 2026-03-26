89fa916e5ed203423605ef157ae22caa66fc9835
M Latief <latief.polcode@gmail.com>
2026-03-26 19:31:02 +0700

    fetch stock data and update UI with live stock information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
-import BuyButton from './components/BuyButton';
-import Stock from './components/Stock';
+import { useEffect, useState } from "react";
+import BuyButton from "./components/BuyButton";
+import FlashSaleTimer from "./components/FlashSaleTimer";
+import AdminPanel from "./components/AdminPanel";
 
 function App() {
+  const [stock, setStock] = useState(null);
+  const [isLive, setIsLive] = useState(false);
+
+  // fetch stock global
+  useEffect(() => {
+    const fetchStock = async () => {
+      const res = await fetch("http://localhost:8080/api/stock");
+      const data = await res.json();
+      setStock(data.stock);
+    };
+
+    fetchStock();
+    const interval = setInterval(fetchStock, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center', marginTop: 50 }}>
+    <div style={{ textAlign: "center", marginTop: 50 }}>
       <h1>🔥 Flash Sale</h1>
-      <Stock />
-      <BuyButton />
+
+      <FlashSaleTimer setIsLive={setIsLive} />
+
+      <h3>Stock: {stock}</h3>
+
+      <BuyButton stock={stock} isLive={isLive} />
+      <AdminPanel />
+
     </div>
   );
 }
