321ef98be765db60727bd4254715fa47e4926e1a
M Latief <latief.polcode@gmail.com>
2026-03-26 21:42:42 +0700

    refactor: update load testing configuration to use ramping VUs scenario

--- k6/test.js
+++ k6/test.js
@@ -1,21 +1,31 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { sleep } from 'k6';
+
+/* try complex scenario
+export const options = {
+  scenarios: {
+    spike: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '2s', target: 1000 },
+        { duration: '5s', target: 1000 },
+        { duration: '2s', target: 0 },
+      ],
+    },
+  },
+};
+*/
 
 export const options = {
-    vus: 200, // virtual users
+    vus: 200,
     duration: '10s',
 };
 
 export default function () {
     const userId = Math.floor(Math.random() * 1000000);
 
-    const res = http.get(
-        `http://nginx/api/buy?user=${userId}`
-    );
-
-    check(res, {
-        'status is 200 or 400': (r) => r.status === 200 || r.status === 400,
-    });
+    http.get(`http://nginx/api/buy?user=${userId}`);
 
     sleep(0.1);
 }
